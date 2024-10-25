ed33411c25ef037e94b1c72256dcdf4d65d112fe
Nicholas shankland <nicholasshankland@Nicholass-MacBook-Pro-2.local>
2024-10-25 13:59:15 +0100

    updated the station and relative weighting

--- ts/build/src/questions/real_world/total_rating_algorithm.js
+++ ts/build/src/questions/real_world/total_rating_algorithm.js
@@ -2,8 +2,74 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const promises_1 = require("node:fs/promises");
 const path = require('node:path');
+const readline = require('node:readline');
 async function main() {
-    const file_path = path.join(__dirname, '/..', '/..', '/..', '/..', '/assets', '/t.csv');
+    // const rl = readline.createInterface({
+    //   input: process.stdin,
+    //   output: process.stdout,
+    // });
+    // rl.question(
+    //   `Which station is this?\n`,
+    //   async (station: string): Promise<void> => {
+    //     console.log(`Calulation for staion: ${station}`);
+    //     await calculate_statuses(station);
+    //     rl.close();
+    //   }
+    // );
+    await calculate_statuses('DSN1');
+}
+const percent_map = {
+    dcr_val: 0.25,
+    dnr_dpmo_val: 0.25,
+    ce_val: 0.2,
+    pod_val: 0.1,
+    cc_val: 0.1,
+    dex_val: 0.1,
+};
+async function calculate_statuses(station) {
+    let dnr_fan = 1100;
+    let dnr_great = 1100;
+    let dnr_fair = 1100;
+    switch (station) {
+        case 'DRG2':
+            dnr_fan = 1100;
+            dnr_great = 1300;
+            dnr_fair = 1650;
+            break;
+        case 'DSN1':
+            dnr_fan = 1100;
+            dnr_great = 1300;
+            dnr_fair = 1700;
+            break;
+        case 'DBS3':
+            dnr_fan = 1300;
+            dnr_great = 1550;
+            dnr_fair = 2000;
+            break;
+        case 'DBS2':
+            dnr_fan = 1400;
+            dnr_great = 1650;
+            dnr_fair = 2100;
+            break;
+        case 'DEX2':
+            dnr_fan = 1050;
+            dnr_great = 1250;
+            dnr_fair = 1600;
+            break;
+        case 'DCF1':
+            dnr_fan = 1200;
+            dnr_great = 1400;
+            dnr_fair = 1800;
+            break;
+        case 'DSA1':
+            dnr_fan = 1200;
+            dnr_great = 1400;
+            dnr_fair = 1850;
+            break;
+        default:
+            throw new Error('Station is not valid, please choose: DRG2, DSN1, DBS3, DBS2, DEX2, DCF1, DSA1');
+    }
+    const file_path = path.join(__dirname, '/..', '/..', '/..', '/..', '/assets', '/DSN1.csv');
     const file = await (0, promises_1.open)(file_path);
     let current_rating = 0;
     let total_count = 0;
@@ -14,6 +80,7 @@ async function main() {
     const fair = 14;
     const poor = 11;
     for await (const line of file.readLines()) {
+        const dont_include = [];
         total_count++;
         current_rating = 0;
         const [_id, status, _total_score, _delivered, dcr, dnr_dpmo, pod, cc, ce, dex, _focus_ares,] = line.split(',');
@@ -21,14 +88,14 @@ async function main() {
             continue;
         }
         let dcr_val = Number(dcr.split('%')[0]);
-        if (_id.includes('M00')) {
-            console.log('moooooooo dcr: ', dcr_val);
+        if (_id.includes('GBWC')) {
+            console.log('DCR VAL: ', dcr_val);
         }
         switch (true) {
             case dcr_val >= 99:
                 dcr_val = fantastic;
                 break;
-            case dcr_val >= 98.5:
+            case dcr_val >= 98.75:
                 dcr_val = great;
                 break;
             case dcr_val >= 98:
@@ -37,21 +104,18 @@ async function main() {
             default:
                 dcr_val = poor;
         }
-        /*
-         * DNR DPMO < 1100 = 4
-         * DNR DPMO < 1300 = 3
-         * DNR DPMO < 1700 = 2
-         * DNR DPMO > 1700 = 1
-         */
+        if (dnr_dpmo == '-') {
+            dont_include.push('dnr_dpmo_val');
+        }
         let dnr_dpmo_val = dnr_dpmo == '-' ? 0 : Number(dnr_dpmo);
         switch (true) {
-            case dnr_dpmo_val < 1100:
+            case dnr_dpmo_val < dnr_fan:
                 dnr_dpmo_val = fantastic;
                 break;
-            case dnr_dpmo_val < 1300:
+            case dnr_dpmo_val < dnr_great:
                 dnr_dpmo_val = great;
                 break;
-            case dnr_dpmo_val < 1700:
+            case dnr_dpmo_val < dnr_fair:
                 dnr_dpmo_val = fair;
                 break;
             default:
@@ -63,9 +127,15 @@ async function main() {
          * POD > 97 = 2
          * POD > -1 = 1
          */
+        if (pod == '-') {
+            dont_include.push('pod_val');
+        }
         let pod_val = pod == '-' ? 100 : Number(pod.split('%')[0]);
+        if (_id.includes('BA0N')) {
+            console.log('pod val: ', pod_val);
+        }
         switch (true) {
-            case pod_val > 99:
+            case pod_val > 98.9:
                 pod_val = fantastic;
                 break;
             case pod_val > 98:
@@ -83,6 +153,9 @@ async function main() {
          * CC > 90 = 2
          * CC > -1 = 1
          */
+        if (cc == '-') {
+            dont_include.push('cc_val');
+        }
         let cc_val = cc == '-' ? 100 : Number(cc.split('%')[0]);
         switch (true) {
             case cc_val > 98:
@@ -103,6 +176,9 @@ async function main() {
          * DEX > 80 = 2
          * DEX > -1 = 1
          */
+        if (dex == '-') {
+            dont_include.push('dex_val');
+        }
         let dex_val = dex == '-' ? 100 : Number(dex.split('%')[0]);
         switch (true) {
             case dex_val > 87:
@@ -137,12 +213,41 @@ async function main() {
           doubling the vals to get a nice 100%
           
         */
-        current_rating += Number((dcr_val * 0.25).toFixed(2));
-        current_rating += Number((dnr_dpmo_val * 0.25).toFixed(2));
-        current_rating += Number((ce_val * 0.2).toFixed(2));
-        current_rating += Number((pod_val * 0.1).toFixed(2));
-        current_rating += Number((cc_val * 0.1).toFixed(2));
-        current_rating += Number((dex_val * 0.1).toFixed(2));
+        let missing_fields = 0;
+        let missing_percent = 0;
+        const options = [
+            'dcr_val',
+            'dnr_dpmo_val',
+            'ce_val',
+            'pod_val',
+            'cc_val',
+            'dex_val',
+        ];
+        for (let i = 0; i < options.length; i++) {
+            if (dont_include.includes(options[i])) {
+                missing_fields++;
+                missing_percent += percent_map[options[i]];
+            }
+        }
+        const multiplicative = Number((1 / (1 - missing_percent)).toFixed(5));
+        if (!dont_include.includes('dcr_val')) {
+            current_rating += Number((dcr_val * (0.25 * multiplicative)).toFixed(2));
+        }
+        if (!dont_include.includes('dnr_dpmo_val')) {
+            current_rating += Number((dnr_dpmo_val * (0.25 * multiplicative)).toFixed(2));
+        }
+        if (!dont_include.includes('ce_val')) {
+            current_rating += Number((ce_val * (0.2 * multiplicative)).toFixed(2));
+        }
+        if (!dont_include.includes('pod_val')) {
+            current_rating += Number((pod_val * (0.1 * multiplicative)).toFixed(2));
+        }
+        if (!dont_include.includes('cc_val')) {
+            current_rating += Number((cc_val * (0.1 * multiplicative)).toFixed(2));
+        }
+        if (!dont_include.includes('dex_val')) {
+            current_rating += Number((dex_val * (0.1 * multiplicative)).toFixed(2));
+        }
         let computed_status = determine_status(current_rating);
         if (computed_status !== status) {
             console.log('computed: ', computed_status, '\tstatus: ', status, '\t rating: ', current_rating, '\t total: ', _total_score, '\tid: ', _id);
@@ -166,7 +271,7 @@ async function main() {
 }
 function determine_status(rating) {
     switch (true) {
-        case rating > 18.55:
+        case rating > 18.62:
             return 'FANTASTIC_PLUS';
         case rating > 17.8:
             return 'FANTASTIC';
--- ts/src/questions/real_world/total_rating_algorithm.ts
+++ ts/src/questions/real_world/total_rating_algorithm.ts
@@ -1,5 +1,6 @@
 import {open} from 'node:fs/promises';
 const path = require('node:path');
+const readline = require('node:readline');
 
 interface WrongObjCount {
   [key: string]: number;
@@ -9,7 +10,81 @@ interface WrongObj {
   [key: string]: WrongObjCount;
 }
 
+interface PercentMap {
+  [key: string]: number;
+}
+
 async function main() {
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  // });
+  // rl.question(
+  //   `Which station is this?\n`,
+  //   async (station: string): Promise<void> => {
+  //     console.log(`Calulation for staion: ${station}`);
+  //     await calculate_statuses(station);
+  //     rl.close();
+  //   }
+  // );
+  await calculate_statuses('DSN1');
+}
+
+const percent_map: PercentMap = {
+  dcr_val: 0.25,
+  dnr_dpmo_val: 0.25,
+  ce_val: 0.2,
+  pod_val: 0.1,
+  cc_val: 0.1,
+  dex_val: 0.1,
+};
+
+async function calculate_statuses(station: string): Promise<void> {
+  let dnr_fan = 1100;
+  let dnr_great = 1100;
+  let dnr_fair = 1100;
+
+  switch (station) {
+    case 'DRG2':
+      dnr_fan = 1100;
+      dnr_great = 1300;
+      dnr_fair = 1650;
+      break;
+    case 'DSN1':
+      dnr_fan = 1100;
+      dnr_great = 1300;
+      dnr_fair = 1700;
+      break;
+    case 'DBS3':
+      dnr_fan = 1300;
+      dnr_great = 1550;
+      dnr_fair = 2000;
+      break;
+    case 'DBS2':
+      dnr_fan = 1400;
+      dnr_great = 1650;
+      dnr_fair = 2100;
+      break;
+    case 'DEX2':
+      dnr_fan = 1050;
+      dnr_great = 1250;
+      dnr_fair = 1600;
+      break;
+    case 'DCF1':
+      dnr_fan = 1200;
+      dnr_great = 1400;
+      dnr_fair = 1800;
+      break;
+    case 'DSA1':
+      dnr_fan = 1200;
+      dnr_great = 1400;
+      dnr_fair = 1850;
+      break;
+    default:
+      throw new Error(
+        'Station is not valid, please choose: DRG2, DSN1, DBS3, DBS2, DEX2, DCF1, DSA1'
+      );
+  }
   const file_path = path.join(
     __dirname,
     '/..',
@@ -17,7 +92,7 @@ async function main() {
     '/..',
     '/..',
     '/assets',
-    '/t.csv'
+    '/DSN1.csv'
   );
 
   const file = await open(file_path);
@@ -33,6 +108,7 @@ async function main() {
   const fair = 14;
   const poor = 11;
   for await (const line of file.readLines()) {
+    const dont_include = [];
     total_count++;
     current_rating = 0;
     const [
@@ -53,14 +129,14 @@ async function main() {
       continue;
     }
     let dcr_val = Number(dcr.split('%')[0]);
-    if (_id.includes('M00')) {
-      console.log('moooooooo dcr: ', dcr_val);
+    if (_id.includes('GBWC')) {
+      console.log('DCR VAL: ', dcr_val);
     }
     switch (true) {
       case dcr_val >= 99:
         dcr_val = fantastic;
         break;
-      case dcr_val >= 98.5:
+      case dcr_val >= 98.75:
         dcr_val = great;
         break;
       case dcr_val >= 98:
@@ -70,22 +146,18 @@ async function main() {
         dcr_val = poor;
     }
 
-    /*
-     * DNR DPMO < 1100 = 4
-     * DNR DPMO < 1300 = 3
-     * DNR DPMO < 1700 = 2
-     * DNR DPMO > 1700 = 1
-     */
-
+    if (dnr_dpmo == '-') {
+      dont_include.push('dnr_dpmo_val');
+    }
     let dnr_dpmo_val = dnr_dpmo == '-' ? 0 : Number(dnr_dpmo);
     switch (true) {
-      case dnr_dpmo_val < 1100:
+      case dnr_dpmo_val < dnr_fan:
         dnr_dpmo_val = fantastic;
         break;
-      case dnr_dpmo_val < 1300:
+      case dnr_dpmo_val < dnr_great:
         dnr_dpmo_val = great;
         break;
-      case dnr_dpmo_val < 1700:
+      case dnr_dpmo_val < dnr_fair:
         dnr_dpmo_val = fair;
         break;
       default:
@@ -99,9 +171,15 @@ async function main() {
      * POD > -1 = 1
      */
 
+    if (pod == '-') {
+      dont_include.push('pod_val');
+    }
     let pod_val = pod == '-' ? 100 : Number(pod.split('%')[0]);
+    if (_id.includes('BA0N')) {
+      console.log('pod val: ', pod_val);
+    }
     switch (true) {
-      case pod_val > 99:
+      case pod_val > 98.9:
         pod_val = fantastic;
         break;
       case pod_val > 98:
@@ -120,7 +198,9 @@ async function main() {
      * CC > 90 = 2
      * CC > -1 = 1
      */
-
+    if (cc == '-') {
+      dont_include.push('cc_val');
+    }
     let cc_val = cc == '-' ? 100 : Number(cc.split('%')[0]);
     switch (true) {
       case cc_val > 98:
@@ -142,7 +222,9 @@ async function main() {
      * DEX > 80 = 2
      * DEX > -1 = 1
      */
-
+    if (dex == '-') {
+      dont_include.push('dex_val');
+    }
     let dex_val = dex == '-' ? 100 : Number(dex.split('%')[0]);
     switch (true) {
       case dex_val > 87:
@@ -179,12 +261,46 @@ async function main() {
       
     */
 
-    current_rating += Number((dcr_val * 0.25).toFixed(2));
-    current_rating += Number((dnr_dpmo_val * 0.25).toFixed(2));
-    current_rating += Number((ce_val * 0.2).toFixed(2));
-    current_rating += Number((pod_val * 0.1).toFixed(2));
-    current_rating += Number((cc_val * 0.1).toFixed(2));
-    current_rating += Number((dex_val * 0.1).toFixed(2));
+    let missing_fields = 0;
+    let missing_percent = 0;
+    const options = [
+      'dcr_val',
+      'dnr_dpmo_val',
+      'ce_val',
+      'pod_val',
+      'cc_val',
+      'dex_val',
+    ];
+
+    for (let i = 0; i < options.length; i++) {
+      if (dont_include.includes(options[i])) {
+        missing_fields++;
+        missing_percent += percent_map[options[i]];
+      }
+    }
+
+    const multiplicative = Number((1 / (1 - missing_percent)).toFixed(5));
+
+    if (!dont_include.includes('dcr_val')) {
+      current_rating += Number((dcr_val * (0.25 * multiplicative)).toFixed(2));
+    }
+    if (!dont_include.includes('dnr_dpmo_val')) {
+      current_rating += Number(
+        (dnr_dpmo_val * (0.25 * multiplicative)).toFixed(2)
+      );
+    }
+    if (!dont_include.includes('ce_val')) {
+      current_rating += Number((ce_val * (0.2 * multiplicative)).toFixed(2));
+    }
+    if (!dont_include.includes('pod_val')) {
+      current_rating += Number((pod_val * (0.1 * multiplicative)).toFixed(2));
+    }
+    if (!dont_include.includes('cc_val')) {
+      current_rating += Number((cc_val * (0.1 * multiplicative)).toFixed(2));
+    }
+    if (!dont_include.includes('dex_val')) {
+      current_rating += Number((dex_val * (0.1 * multiplicative)).toFixed(2));
+    }
 
     let computed_status = determine_status(current_rating);
     if (computed_status !== status) {
@@ -241,7 +357,7 @@ async function main() {
 }
 function determine_status(rating: number): string {
   switch (true) {
-    case rating > 18.55:
+    case rating > 18.62:
       return 'FANTASTIC_PLUS';
     case rating > 17.8:
       return 'FANTASTIC';
