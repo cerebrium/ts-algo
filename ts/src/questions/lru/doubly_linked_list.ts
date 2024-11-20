151ea683f66093bf01901bcbcd6f4ae9607715bd
Nicholas shankland <nicholasshankland@Nicholass-MacBook-Pro-2.local>
2024-11-20 11:12:02 +0000

    implemented the lru, need to test it

---
+++ ts/src/questions/lru/doubly_linked_list.ts
@@ -0,0 +1,178 @@
+interface DoublyLinkedNode<T> {
+  id: number;
+  value: T;
+  prev: DoublyLinkedNode<T> | null;
+  next: DoublyLinkedNode<T> | null;
+}
+
+export class DoublyLinkedList<T> {
+  private head: DoublyLinkedNode<T> | null;
+  private tail: DoublyLinkedNode<T> | null;
+  private max_length: number;
+
+  public id_map: WeakMap<Number, DoublyLinkedNode<T>>;
+  public length: number;
+
+  constructor(max_length: number) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+    this.id_map = new WeakMap<Number, DoublyLinkedNode<T>>();
+    this.max_length = max_length;
+  }
+
+  public push(value: T, id: number): void {
+    this.length++;
+
+    if (this.tail) {
+      this.tail.next = {value, prev: this.tail, next: null, id};
+      this.tail = this.tail.next;
+
+      // This will just delete the node if it's oversized. Not a
+      // needed method for this paticular implementation
+      this.handle_new_value_map(id, this.tail);
+      return;
+    }
+
+    // If no tail, no head either
+    this.head = this.tail = {
+      value,
+      prev: this.tail,
+      next: null,
+      id,
+    };
+
+    this.handle_new_value_map(id, this.tail);
+  }
+
+  public unshift_and_create(value: T, id: number): void {
+    this.length++;
+
+    if (this.head) {
+      this.head.prev = {value, prev: this.tail, next: null, id};
+      this.head = this.head.prev;
+
+      this.handle_new_value_map(id, this.head);
+      return;
+    }
+
+    this.head = this.tail = {
+      value,
+      prev: this.tail,
+      next: null,
+      id,
+    };
+
+    this.handle_new_value_map(id, this.head);
+  }
+
+  public unshift(node: DoublyLinkedNode<T>): void {
+    if (this.head) {
+      node.next = this.head;
+      this.head = node;
+
+      this.handle_new_value_map(node.id, node);
+      return;
+    }
+
+    this.head = this.tail = node;
+    this.handle_new_value_map(node.id, node);
+  }
+
+  private pop(): T | null {
+    if (!this.length) return null;
+    this.length--;
+
+    if (this.tail && this.tail.prev) {
+      const node_value = this.tail.value;
+
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+
+      return node_value;
+    }
+
+    // If there was not tail, length check would fail
+    // there is no prev, which means tail = head
+    let node_value = this.tail?.value;
+    this.tail = null;
+    this.head = null;
+
+    // This just makes ts happy
+    return node_value ? node_value : null;
+  }
+
+  private shift(): T | null {
+    if (!this.length) return null;
+    this.length--;
+
+    if (this.head && this.head.next) {
+      const current_node = this.head.value;
+      // gc will collect since previous head node has no pointers
+      // to it.
+      this.head = this.head.next;
+
+      return current_node;
+    }
+
+    // If there was not tail, length check would fail
+    // there is no prev, which means tail = head
+    const node_value = this.head?.value;
+    this.tail = null;
+    this.head = null;
+
+    // This just makes ts happy
+    return node_value ? node_value : null;
+  }
+
+  private handle_new_value_map(
+    id: number,
+    new_node: DoublyLinkedNode<T>
+  ): void {
+    this.id_map.set(id, new_node);
+
+    if (this.length > this.max_length) {
+      // Can null coerce due to above check.
+      this.tail = this.tail?.prev!;
+      this.tail.next = null;
+    }
+  }
+
+  public retrieve_node(id: number): DoublyLinkedNode<T> | null {
+    const node = this.id_map.get(id);
+
+    if (!node) return null;
+
+    // Remove from the weak map
+    this.id_map.delete(id);
+
+    // Need do detach the node
+    if (this.head?.id !== node.id && this.tail?.id !== node.id) {
+      node.prev = node.next;
+      this.length--;
+      return node;
+    }
+
+    if (this.head?.id === node.id) {
+      if (this.length > 1) {
+        this.head = this.head.next;
+        this.length--;
+        return node;
+      }
+
+      this.head = this.tail = null;
+      this.length--;
+      return node;
+    }
+
+    if (this.length > 1) {
+      this.tail = this.tail?.prev!;
+      this.length--;
+      return node;
+    }
+
+    this.head = this.tail = null;
+    this.length--;
+    return node;
+  }
+}
---
+++ ts/src/questions/lru/lru.ts
@@ -0,0 +1,35 @@
+import {DoublyLinkedList} from './doubly_linked_list';
+
+export interface Entity {
+  id: number;
+  [key: string]: any;
+}
+
+export class LRU {
+  cache: DoublyLinkedList<Entity>;
+
+  constructor(first_item: Entity, length: number) {
+    this.cache = new DoublyLinkedList<Entity>(length);
+    this.cache.unshift_and_create(first_item, first_item.id);
+  }
+
+  public add_item(item: Entity): void {
+    const cached_item = this.cache.retrieve_node(item.id);
+
+    if (cached_item) {
+      this.cache.unshift_and_create(item, item.id);
+      return;
+    }
+
+    this.cache.unshift_and_create(item, item.id);
+  }
+
+  public retrieve_item(id: number): Entity | null {
+    const cached_item = this.cache.retrieve_node(id);
+
+    if (!cached_item) return null;
+    this.cache.unshift(cached_item);
+
+    return cached_item.value;
+  }
+}
