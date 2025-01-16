b230caeb43104bd684912c6556ca288ca53164d1
Nicholas shankland <nicholasshankland@Nicholass-MacBook-Pro-2.local>
2025-01-16 17:44:01 +0000

    implementing heap for alphabetical trie

--- ts/src/questions/leet_code/list_of_names.ts
+++ ts/src/questions/leet_code/list_of_names.ts
@@ -106,3 +106,139 @@ export class ListOfWords {
     });
   }
 }
+
+// Min heap implementation
+
+class MinHeap<T> {
+  data: Array<T> = [];
+
+  public add(val: T): void {
+    this.data.push(val);
+    if (this.data.length < 2) {
+      return;
+    }
+
+    this.bubble_up();
+  }
+
+  public pop(): T | null {
+    if (this.data.length < 1) {
+      return null;
+    }
+    const val_to_return = this.data[0];
+
+    if (this.data.length < 3) {
+    }
+
+    return val_to_return;
+  }
+
+  private heapify_down() {
+    let parent_idx: number = 0;
+    let smallest_child_idx = this.get_lowest_child(
+      this.get_left_child(parent_idx),
+      this.get_right_child(parent_idx)
+    );
+
+    if (!smallest_child_idx) {
+      return;
+    }
+
+    while (this.data[smallest_child_idx] < this.data[parent_idx]) {
+      this.swap(smallest_child_idx, parent_idx);
+      const new_smallest_child_idx = this.get_lowest_child(
+        this.get_left_child(smallest_child_idx),
+        this.get_right_child(smallest_child_idx)
+      );
+
+      if (!new_smallest_child_idx) {
+        return;
+      }
+
+      parent_idx = smallest_child_idx;
+      smallest_child_idx = new_smallest_child_idx;
+    }
+  }
+
+  private bubble_up() {
+    let current_idx: number = this.data.length - 1;
+    let parent_idx = this.get_parent(current_idx);
+
+    if (!parent_idx) {
+      return;
+    }
+
+    while (this.data[current_idx] < this.data[parent_idx]) {
+      this.swap(current_idx, parent_idx);
+      const new_parent_idx = this.get_parent(parent_idx);
+      if (!new_parent_idx) {
+        return;
+      }
+
+      current_idx = parent_idx;
+      parent_idx = new_parent_idx;
+    }
+  }
+  // Returns idx of child
+  private get_left_child(parent_idx: number): null | number {
+    const left_child_idx = 2 * parent_idx;
+
+    if (left_child_idx > this.data.length - 1) {
+      return null;
+    }
+
+    return left_child_idx;
+  }
+
+  // Returns idx of child
+  private get_right_child(parent_idx: number): null | number {
+    const right_child_idx = 2 * parent_idx + 1;
+
+    if (right_child_idx > this.data.length - 1) {
+      return null;
+    }
+
+    return right_child_idx;
+  }
+
+  // Returns idx of parent
+  private get_parent(child_idx: number): null | number {
+    const parent_idx = Math.floor((child_idx - 1) / 2);
+
+    if (parent_idx < 0) {
+      return null;
+    }
+
+    return parent_idx;
+  }
+
+  private get_lowest_child(
+    child_idx_one: number | null,
+    child_idx_two: number | null
+  ): number | null {
+    if (!child_idx_one && !child_idx_two) {
+      return null;
+    }
+
+    if (!child_idx_one) {
+      return child_idx_two;
+    }
+
+    if (!child_idx_two) {
+      return child_idx_one;
+    }
+
+    if (this.data[child_idx_one] < this.data[child_idx_two]) {
+      return child_idx_one;
+    }
+
+    return child_idx_two;
+  }
+
+  private swap(idx_one: number, idx_two: number): void {
+    [this.data[idx_one], this.data[idx_two]] = [
+      this.data[idx_two],
+      this.data[idx_one],
+    ];
+  }
+}
